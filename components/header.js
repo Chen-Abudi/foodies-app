2cea2e8b52c79441ff36e4451ac3675100ed0c60
Grace Chen Abudi <cheezumcali23@gmail.com>
2024-05-28 12:48:23 +0300

    feat: Add an header (#2)

---
+++ components/header.js
@@ -0,0 +1,10 @@
+function Header() {
+  return (
+    <>
+      <img src="/logo.png" alt="A server surrounded by magic sparkles." />
+      <h1>Welcome to this NextJS Course!</h1>
+    </>
+  );
+}
+
+export default Header;
