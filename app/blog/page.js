f83131730f42d0070aeedfaa0f02c7c1bd96511b
Grace Chen Abudi <cheezumcali23@gmail.com>
2024-05-28 16:09:09 +0300

    feat: Add example blog route

---
+++ app/blog/page.js
@@ -0,0 +1,17 @@
+import Link from "next/link";
+
+function BlogPage() {
+  return (
+    <main>
+      <h1>The Blog</h1>
+      <p>
+        <Link href="/blog/post-1">Post 1</Link>
+      </p>
+      <p>
+        <Link href="/blog/post-2">Post 2</Link>
+      </p>
+    </main>
+  );
+}
+
+export default BlogPage;
