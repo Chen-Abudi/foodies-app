f7749d1ae59299c83f6f81174f3d0a432f5fba22
Grace Chen Abudi <cheezumcali23@gmail.com>
2024-05-30 23:18:42 +0300

    chore: Clear comments

--- app/page.js
+++ app/page.js
@@ -47,19 +47,5 @@ export default function Home() {
         </section>
       </main>
     </>
-    // <main>
-    //   <h1 style={{ color: "white", textAlign: "center" }}>
-    //     Time to get started!
-    //   </h1>
-    //   <p>
-    //     <Link href="/meals">Our Meals</Link>
-    //   </p>
-    //   <p>
-    //     <Link href="/meals/share">Share A Meal</Link>
-    //   </p>
-    //   <p>
-    //     <Link href="/community">Our Community</Link>
-    //   </p>
-    // </main>
   );
 }
