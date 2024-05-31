16981ad32189555773f90856186000e8729ea519
Grace Chen Abudi <cheezumcali23@gmail.com>
2024-05-31 23:09:06 +0300

    feat: Add community page (#8)

--- components/main-header/main-header.js
+++ components/main-header/main-header.js
@@ -1,11 +1,15 @@
 import Link from "next/link";
 import Image from "next/image";
+// import { usePathname } from "next/navigation";
 
 import MainHeaderBackground from "./main-header-background";
 import HeaderLogo from "@/assets/logo.png";
 import classes from "./main-header.module.css";
+import NavLink from "./nav-link";
 
 function MainHeader() {
+  // const path = usePathname();
+
   return (
     <>
       <MainHeaderBackground />
@@ -18,10 +22,24 @@ function MainHeader() {
         <nav className={classes.nav}>
           <ul>
             <li>
-              <Link href="/meals">Browse Meals</Link>
+              <NavLink href="/meals">Browse Meals</NavLink>
+              {/* <Link
+                href="/meals"
+                className={
+                  path.startsWith("/meals") ? classes.active : undefined
+                }
+              >
+                Browse Meals
+              </Link> */}
             </li>
             <li>
-              <Link href="/community">Foodies Community</Link>
+              <NavLink href="/community">Foodies Community</NavLink>
+              {/* <Link
+                href="/community"
+                className={path === "/community" ? classes.active : undefined}
+              >
+                Foodies Community
+              </Link> */}
             </li>
           </ul>
         </nav>
