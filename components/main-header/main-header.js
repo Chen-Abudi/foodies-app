418cebd552921eb12318d682f15ba47d859c0f90
Grace Chen Abudi <cheezumcali23@gmail.com>
2024-05-31 23:10:31 +0300

    chore: Clear comments

--- components/main-header/main-header.js
+++ components/main-header/main-header.js
@@ -1,6 +1,5 @@
 import Link from "next/link";
 import Image from "next/image";
-// import { usePathname } from "next/navigation";
 
 import MainHeaderBackground from "./main-header-background";
 import HeaderLogo from "@/assets/logo.png";
@@ -8,8 +7,6 @@ import classes from "./main-header.module.css";
 import NavLink from "./nav-link";
 
 function MainHeader() {
-  // const path = usePathname();
-
   return (
     <>
       <MainHeaderBackground />
@@ -23,23 +20,9 @@ function MainHeader() {
           <ul>
             <li>
               <NavLink href="/meals">Browse Meals</NavLink>
-              {/* <Link
-                href="/meals"
-                className={
-                  path.startsWith("/meals") ? classes.active : undefined
-                }
-              >
-                Browse Meals
-              </Link> */}
             </li>
             <li>
               <NavLink href="/community">Foodies Community</NavLink>
-              {/* <Link
-                href="/community"
-                className={path === "/community" ? classes.active : undefined}
-              >
-                Foodies Community
-              </Link> */}
             </li>
           </ul>
         </nav>
