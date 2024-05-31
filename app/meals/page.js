db3a4dc2ab3e2ffec3614a4a503b0f6b1a8edfb7
Grace Chen Abudi <cheezumcali23@gmail.com>
2024-05-31 23:48:03 +0300

    feat: Add meal item

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,7 +1,29 @@
+// import Link from "next/link";
 import Link from "next/link";
 
+import MealsGrid from "@/components/meals/meals-grid";
+import classes from "./page.module.css";
+
 function MealsPage() {
-  return <h1>Meals Page</h1>;
+  return (
+    <>
+      <header className={classes.header}>
+        <h1>
+          Delicious meals, created{" "}
+          <span className={classes.highlight}>by you</span>
+        </h1>
+        <p>
+          Choose your favorite recipe and cook it yourself. It is easy and fun!
+        </p>
+        <p className={classes.cta}>
+          <Link href="/meals/share">Share Your Favorite Recipe</Link>
+        </p>
+      </header>
+      <main className={classes.main}>
+        <MealsGrid meals={[]} />
+      </main>
+    </>
+  );
 }
 
 export default MealsPage;
