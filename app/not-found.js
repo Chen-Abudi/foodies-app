bf9e763ff94ddcae0ee27e24a12f8093b11cf252
Grace Chen Abudi <cheezumcali23@gmail.com>
2024-06-04 13:15:30 +0300

    feat: Add not found page (#12)

---
+++ app/not-found.js
@@ -0,0 +1,10 @@
+function NotFoundPage() {
+  return (
+    <main className="not-found">
+      <h1>Not Found</h1>
+      <p>Unfortunately, we could not find the requested page or resource.</p>
+    </main>
+  );
+}
+
+export default NotFoundPage;
