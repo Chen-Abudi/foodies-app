bd75a754c39037704cc0031bad732fa0cce59e7a
Grace Chen Abudi <cheezumcali23@gmail.com>
2024-06-04 18:28:36 +0300

    feat: Handle not found error for meal

--- app/meals/[mealSlug]/page.js
+++ app/meals/[mealSlug]/page.js
@@ -1,10 +1,15 @@
 import Image from "next/image";
+import { notFound } from "next/navigation";
 
 import classes from "./page.module.css";
 import { getMeal } from "@/lib/meals";
 
 function MealDetailsPage({ params }) {
   const meal = getMeal(params.mealSlug);
+
+  if (!meal) {
+    notFound();
+  }
 
   meal.instructions = meal.instructions.replace(/\n/g, "<br />");
 
