7570dbc205a17a3ed6490d4d6d527cd41916386f
Grace Chen Abudi <cheezumcali23@gmail.com>
2024-06-04 14:12:26 +0300

    feat: Add meal details page

--- app/meals/[mealSlug]/page.js
+++ app/meals/[mealSlug]/page.js
@@ -1,5 +1,43 @@
-function MealPage() {
-  return <h1>Meals Details</h1>;
+import Image from "next/image";
+
+import classes from "./page.module.css";
+import { getMeal } from "@/lib/meals";
+
+function MealDetailsPage({ params }) {
+  const meal = getMeal(params.mealSlug);
+
+  meal.instructions = meal.instructions.replace(/\n/g, "<br />");
+
+  return (
+    <>
+      <header className={classes.header}>
+        <div className={classes.image}>
+          <Image
+            src={meal.image}
+            alt={meal.title}
+            fill
+            priority
+            // sizes="20rem"
+          />
+        </div>
+        <div className={classes.headerText}>
+          <h1>{meal.title}</h1>
+          <p className={classes.creator}>
+            by <a href={`mailto:${meal.creator_email}`}>{meal.creator}</a>
+          </p>
+          <p className={classes.summary}>{meal.summary}</p>
+        </div>
+      </header>
+      <main>
+        <p
+          className={classes.instructions}
+          dangerouslySetInnerHTML={{
+            __html: meal.instructions,
+          }}
+        ></p>
+      </main>
+    </>
+  );
 }
 
-export default MealPage;
+export default MealDetailsPage;
