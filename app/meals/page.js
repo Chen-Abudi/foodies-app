a6298bb77a0bd3624157c0d8c036a40fa1653266
Grace Chen Abudi <cheezumcali23@gmail.com>
2024-06-04 10:57:17 +0300

    refactor: Outsource meals data fetching

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,11 +1,18 @@
+import { Suspense } from "react";
 import Link from "next/link";
 
 import MealsGrid from "@/components/meals/meals-grid";
 import classes from "./page.module.css";
 import { getMeals } from "@/lib/meals";
 
-async function MealsPage() {
+async function Meals() {
   const meals = await getMeals();
+
+  return <MealsGrid meals={meals} />;
+}
+
+function MealsPage() {
+  // const meals = await getMeals();
 
   return (
     <>
@@ -22,7 +29,12 @@ async function MealsPage() {
         </p>
       </header>
       <main className={classes.main}>
-        <MealsGrid meals={meals} />
+        {/* <MealsGrid meals={meals} /> */}
+        <Suspense
+          fallback={<p className={classes.loading}>Fetching meals...</p>}
+        >
+          <Meals />
+        </Suspense>
       </main>
     </>
   );
