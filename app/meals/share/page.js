26c52f4da383e63304dae2df8638faf33b6b006f
Grace Chen Abudi <cheezumcali23@gmail.com>
2024-06-05 17:12:04 +0300

    feat: Add form submission

--- app/meals/share/page.js
+++ app/meals/share/page.js
@@ -3,6 +3,21 @@ import ImagePicker from "@/components/meals/image-picker";
 import classes from "./page.module.css";
 
 function ShareMealPage() {
+  async function shareMeal(formData) {
+    "use server";
+
+    const meal = {
+      title: formData.get("title"),
+      summary: formData.get("summary"),
+      instructions: formData.get("instructions"),
+      image: formData.get("image"),
+      creator: formData.get("name"),
+      creator_email: formData.get("email"),
+    };
+
+    console.log(meal);
+  }
+
   return (
     <>
       <header className={classes.header}>
@@ -13,7 +28,7 @@ function ShareMealPage() {
       </header>
 
       <main className={classes.main}>
-        <form className={classes.form}>
+        <form className={classes.form} action={shareMeal}>
           <div className={classes.row}>
             <p>
               <label htmlFor="name">Your name</label>
@@ -41,7 +56,7 @@ function ShareMealPage() {
               required
             ></textarea>
           </p>
-          <ImagePicker />
+          <ImagePicker label="Your image" name="image" />
           <p className={classes.actions}>
             <button type="submit">Share Meal</button>
           </p>
