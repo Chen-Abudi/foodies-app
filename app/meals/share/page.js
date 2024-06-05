0f1348643ca0686d11211783b4fcf729bc030381
Grace Chen Abudi <cheezumcali23@gmail.com>
2024-06-05 17:25:42 +0300

    chore: Clear comments

--- app/meals/share/page.js
+++ app/meals/share/page.js
@@ -5,21 +5,6 @@ import { shareMeal } from "@/lib/actions";
 import classes from "./page.module.css";
 
 function ShareMealPage() {
-  // async function shareMeal(formData) {
-  //   "use server";
-
-  //   const meal = {
-  //     title: formData.get("title"),
-  //     summary: formData.get("summary"),
-  //     instructions: formData.get("instructions"),
-  //     image: formData.get("image"),
-  //     creator: formData.get("name"),
-  //     creator_email: formData.get("email"),
-  //   };
-
-  //   console.log(meal);
-  // }
-
   return (
     <>
       <header className={classes.header}>
