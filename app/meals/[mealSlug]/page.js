3465453c6d1f6150ef46781b716d5e66c4cb659c
Grace Chen Abudi <cheezumcali23@gmail.com>
2024-06-07 00:01:33 +0300

    feat: Add dynamic metadata

--- app/meals/[mealSlug]/page.js
+++ app/meals/[mealSlug]/page.js
@@ -4,6 +4,19 @@ import { notFound } from "next/navigation";
 import classes from "./page.module.css";
 import { getMeal } from "@/lib/meals";
 
+export async function generateMetadata({ params }) {
+  const meal = getMeal(params.mealSlug);
+
+  if (!meal) {
+    notFound();
+  }
+
+  return {
+    title: meal.title,
+    description: meal.summary,
+  };
+}
+
 function MealDetailsPage({ params }) {
   const meal = getMeal(params.mealSlug);
 
