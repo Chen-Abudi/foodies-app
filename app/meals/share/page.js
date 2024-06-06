c466581363f6ff6561e426ac9524ad5f6e143eab
Grace Chen Abudi <cheezumcali23@gmail.com>
2024-06-06 16:47:09 +0300

    feat: Manage form submission status

--- app/meals/share/page.js
+++ app/meals/share/page.js
@@ -1,6 +1,6 @@
 import ImagePicker from "@/components/meals/image-picker";
-
 import { shareMeal } from "@/lib/actions";
+import MealsFormSubmit from "@/components/meals/meals-form-submit";
 
 import classes from "./page.module.css";
 
@@ -45,7 +45,8 @@ function ShareMealPage() {
           </p>
           <ImagePicker label="Your image" name="image" />
           <p className={classes.actions}>
-            <button type="submit">Share Meal</button>
+            <MealsFormSubmit />
+            {/* <button type="submit">Share Meal</button> */}
           </p>
         </form>
       </main>
