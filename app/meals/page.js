4b158953ec1102b70aad3655489b7bcb820e860a
Grace Chen Abudi <cheezumcali23@gmail.com>
2024-06-06 23:56:28 +0300

    feat: Add static metadata

--- app/meals/page.js
+++ app/meals/page.js
@@ -5,6 +5,11 @@ import MealsGrid from "@/components/meals/meals-grid";
 import classes from "./page.module.css";
 import { getMeals } from "@/lib/meals";
 
+export const metadata = {
+  title: "All Meals",
+  description: "Browse the delicious meals shared by our vibrant community.",
+};
+
 async function Meals() {
   const meals = await getMeals();
 
